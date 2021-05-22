0998dc973dc209d883f14b45b587ac0db5a7ae86
alicia <jiandanfifi>
2021-05-22 23:59:58 +0800

    Initial commit

---
+++ src/main/java/com/lesson/BookShopWebApplication.java
@@ -0,0 +1,18 @@
+package com.lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * BookShopAdminApplication
+ *
+ * @author Alicia
+ * @description
+ * @date 2021/5/22
+ */
+@SpringBootApplication
+public class BookShopWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookShopWebApplication.class, args);
+    }
+}
